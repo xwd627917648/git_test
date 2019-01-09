f37856487ae512a8a41785e61a4251b73609c796
xueweidong <627917648@qq.com>
2019-01-09 09:48:01 +0800

    the second time

---
+++ test_git/src/cn/itcast/GitTest.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/**
+ * @author weidong Xue
+ * @date 2019/1/9  9:41
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+    }
+}
